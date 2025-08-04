52df833430e8482bf54a43af3f7979c830c81ab9
Vincenzo Grillone <vincenzo_1996@hotmail.com>
2025-08-04 16:21:01 -0400

    Built create page

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,8 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
+app.use(cors({
+  orgin: 'http://localhost:5173',
+}))
 app.use(express.json())
 app.use(rateLimiter)
+
 
 app.use("/api/notes", notesRoutes)
 
